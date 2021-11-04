96c09723002fb619725691c3a2a2768051c00c6d
kartik <kabakshi@hotmail.ca>
2021-11-04 18:36:59 -0400

    test fetchISSFlyOverTimes

--- index.js
+++ index.js
@@ -8,6 +8,15 @@ fetchCoordsByIP('99.247.121.151', function(error, coordinates){
     console.log('there is an error', error)
     return
   }
-  console.log('coordinates are', coordinates)
+  console.log('coordinates are', { latitude: '49.27670', longitude: '-123.13000' })
 
 }) // end of fetchCoordsByIP
+
+
+// fetchISSFlyOverTimes({ latitude: '49.27670', longitude: '-123.13000' }, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned flyover times:' , passTimes);
+// })
