e34f7ab2b5aa2a0f288ab02d0771b4e8f21752df
kartik <kabakshi@hotmail.ca>
2021-11-04 18:01:37 -0400

    iss
    implement function

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const {fetchMyIP} = require('./fetchMyIp')
-const {fetchCoordsByIP} = require('./ffetchCoordsByIP')
+const {fetchCoordsByIP} = require('./fetchCoordsByIP')
 
 //  require and run our main fetch function.const { fetchMyIP } = require('./iss');
-
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned IP:' , ip);
-// });
 
 fetchCoordsByIP('99.247.121.151', function(error, coordinates){
   if(error){
