874dde04fc3f6b1070020481a4506003a95a7337
kartik <kabakshi@hotmail.ca>
2021-11-05 15:10:55 -0400

    iss_promised

--- iss_promised.js
+++ iss_promised.js
@@ -1,23 +1,24 @@
 // iss_promised.js
 const request = require('request-promise-native');
 
+
 const fetchMyIP = function() {
   return request('https://api.ipify.org?format=json');
 };
 
 const fetchCoordsByIP = function(body) {
-  const ip = JSON.parse(body).ip;
-  return request(`https://freegeoip.app/json/${ip}`);
 
+  const ip = JSON.parse(body).ip;
+  
+  return request(`https://ipvigilante.com/${ip}`);
 };
 
 const fetchISSFlyOverTimes = function(body) {
-  const { latitude, longitude } = JSON.parse(body);
-  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+
+  let { latitude, longitude } = JSON.parse(body).data;
+  let url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
   return request(url);
 };
 
 module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes };
-module.exports = { fetchMyIP };
-module.exports = { fetchCoordsByIP }
 
